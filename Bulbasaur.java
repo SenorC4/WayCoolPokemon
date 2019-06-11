dfe57ac074cb6fea0702ad66a9a42638bb29b9af
SenorC4 <44325992+SenorC4@users.noreply.github.com>
2019-06-10 21:05:29 -0700

    Create Bulbasaur.java

---
+++ Bulbasaur.java
@@ -0,0 +1,10 @@
+public class Bulbasaur{
+    
+    public String name = "Bulbasaur";
+    public String type = "Grass";
+    public double hp = 45;
+    public double attack = 49;
+    public double defense = 49;
+    public String moveSet[] = {"Tackle", "Razor Leaf"};
+
+}
