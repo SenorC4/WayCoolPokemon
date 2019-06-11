72b0829b8c860201378ceaf5443cc6fa3a11cd3e
SenorC4 <44325992+SenorC4@users.noreply.github.com>
2019-06-10 21:06:12 -0700

    Create Squirtle.java

---
+++ Squirtle.java
@@ -0,0 +1,11 @@
+public class Squirtle{
+    
+    public String name = "Squirtle";
+    public String type = "Water";
+    public double hp = 44;
+    public double attack = 48;
+    public double defense = 65;
+    public String moveSet[] = {"Tackle", "Water Gun"};
+    
+}
+
