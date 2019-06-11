d760c38859f70df85dd34fcd3f60928b759d2ad5
SenorC4 <44325992+SenorC4@users.noreply.github.com>
2019-06-10 21:08:26 -0700

    Create Rattata.java

---
+++ Rattata.java
@@ -0,0 +1,11 @@
+public class Rattata{
+    
+    public String name = "Rattata";
+    public String type = "Normal";
+    public double hp = 30;
+    public double attack = 56;
+    public double defense = 35;
+    public String moveSet[] = {"Tackle", "Mud Slap"};
+
+}
+
