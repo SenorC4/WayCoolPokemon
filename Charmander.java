1d4b389abbbf1d5635e8cbaed3be3be766656361
SenorC4 <44325992+SenorC4@users.noreply.github.com>
2019-06-10 21:07:33 -0700

    Create Charmander.java

---
+++ Charmander.java
@@ -0,0 +1,11 @@
+public class Charmander{
+    
+    public String name = "Charmander";
+    public String type = "Fire";
+    public double hp = 39;
+    public double attack = 52;
+    public double defense = 43;
+    public String moveSet[] = {"Tackle", "Ember"};
+    
+}
+
