98a428f333a1063739f7e37a386413663105e6fc
SenorC4 <44325992+SenorC4@users.noreply.github.com>
2019-06-10 21:07:06 -0700

    Create Pikachu.java

---
+++ Pikachu.java
@@ -0,0 +1,10 @@
+public class Pikachu{
+    
+    public String name = "Pikachu";
+    public String type = "Electric";
+    public double hp = 35;
+    public double attack = 55;
+    public double defense = 40;
+    public String moveSet[] = {"Tackle", "Spark"};
+    
+}
